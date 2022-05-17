35582645dacf3a91ab0bda4e49cb9c25f65e7140
salima5214 <mingming5214@gmail.com>
2022-05-17 12:27:53 +0800

    names of Allah

---
+++ media/js/voice_字母.js
@@ -0,0 +1,182 @@
+
+function voice_alif(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/alif.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+function voice_baa(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/baa.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+
+function voice_taa(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/taa.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+
+function voice_thaa(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/thaa.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+function voice_jeem(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/jeem.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+function voice_haa(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/haa.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+
+function voice_khaw(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/khaw.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+function voice_dhaal(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/dhaal.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+
+function voice_zhaal(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/zhaal.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+function voice_raw(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/raw.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+
+function voice_zaa(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/zaa.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+function voice_seen(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/seen.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+function voice_sheen(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/sheen.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+
+function voice_sawd(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/sawd.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+function voice_dhawd(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/dhawd.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+function voice_taw(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/taw.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+
+function voice_zhaw(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/zhaw.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+function voice_ayn(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/ayn.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+function voice_ghayn(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/ghayn.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+function voice_faa(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/faa.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+function voice_qawf(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/qawf.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+function voice_kaaf(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/kaaf.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+function voice_laam(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/laam.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+function voice_meem(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/meem.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+function voice_noon(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/noon.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+
+function voice_wow(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/wow.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+function voice_haaa(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/haaa.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+function voice_hamza(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/hamza.aac";
+    audio.playbackRate = 1;audio.play();
+}
+
+function voice_yaa(){
+    var audio = document.createElement("audio");
+    audio.src = "media/voices/yaa.aac";
+    audio.playbackRate = 1;audio.play();
+}
